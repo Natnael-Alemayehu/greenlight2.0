e3bf465f86ade091b764204437d890f3f5cf3fd5
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-12-31 15:12:42 +0300

    helper writeJSON is now implemented with iterator introduced in 1.23

---
+++ cmd/api/helper.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"maps"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIdParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, header http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Insert(w.Header(), maps.All(header))
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
