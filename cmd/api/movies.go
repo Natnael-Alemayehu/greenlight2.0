11c53984d602b2ed4ee188a5c72cd0e0c0d3565e
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2025-01-01 12:29:30 +0300

    Movie struct implemented.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/natnael-alemayehu/greenlight2.0/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show movie with id: %d", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Persumed Inocent",
+		Runtime:   102,
+		Genres:    []string{"Thriller", "Drama"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusAccepted, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered an error and could not process your request", http.StatusInternalServerError)
+	}
+
 }
